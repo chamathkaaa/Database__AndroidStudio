2b57b4fbe895b01c71dbccf69fcfdfec1cec9c20
UviniC <uviniwijesinghe@gmail.com>
2019-09-28 18:17:43 +0530

    insert

--- app/src/main/java/com/example/database_androidstudio/Database/DBHelper.java
+++ app/src/main/java/com/example/database_androidstudio/Database/DBHelper.java
@@ -14,152 +14,51 @@ public class DBHelper extends SQLiteOpenHelper {
 
     public static final String DATABASE_NAME = "UserInfo.db";
 
+    public static final String TABLE_NAME = "users";
+    public static final String COL_ID = "username";
+    public static final String COL_USERNAME = "username";
+    public static final String COL_PASSWORD = "password";
+
     public DBHelper(Context context){
-        super(context,DATABASE_NAME, null,1);
+        super(context,DATABASE_NAME,null,1);
+        SQLiteDatabase db = this.getWritableDatabase();
+
+    }
+
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        sqLiteDatabase.execSQL("CREATE TABLE " +TABLE_NAME+ "(ID INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT, password TEXT)");
     }
 
     @Override
-    public void onCreate(SQLiteDatabase db) {
-
-        String SQL_CREATE_ENTRIES =
-                "CREATE TABLE " + UsersMaster.Users.TABLE_NAME + "(" +
-                        UsersMaster.Users._ID + "INTEGER PRIMARY KEY," +
-                        UsersMaster.Users.COLUMN_NAME_USERNAME + "TEXT," +
-                        UsersMaster.Users.COLUMN_NAME_PASSWORD + "TEXT)";
-
-        db.execSQL(SQL_CREATE_ENTRIES);
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " +TABLE_NAME);
+        onCreate(sqLiteDatabase);
     }
 
-    @Override
-    public void onUpgrade(SQLiteDatabase db, int i, int i1) {
-
-    }
-
-    public long addInfo(String userName,String password){
+    //insert values
+    public boolean addInfo(Users obj){
         SQLiteDatabase db = getWritableDatabase();
 
         ContentValues values = new ContentValues();
-        values.put(UsersMaster.Users.COLUMN_NAME_USERNAME,userName);
-        values.put(UsersMaster.Users.COLUMN_NAME_PASSWORD,password);
+        values.put(DBHelper.COL_USERNAME, obj.getUsername());
+        values.put(DBHelper.COL_PASSWORD, obj.getPassword());
 
-        long newRowId = db.insert(UsersMaster.Users.TABLE_NAME,null,values);
+        long  result = db.insert(TABLE_NAME,null,values);
 
-        return newRowId;
+        if(result > 0){
+            return true;
+        }else{
+            return false;
+        }
     }
 
-    public void updateInfo(String userName, String password){
-
-        SQLiteDatabase db = getReadableDatabase();
+    public boolean updateInfo(int id, String username, String password){
+        SQLiteDatabase db = getWritableDatabase();
 
         ContentValues values = new ContentValues();
-        values.put(UsersMaster.Users.COLUMN_NAME_PASSWORD,password);
 
-        String selection = UsersMaster.Users.COLUMN_NAME_USERNAME + "LIKE ?";
-        String[] selectionArgs = {userName};
-
-        int count = db.update(
-                UsersMaster.Users.TABLE_NAME,
-                values,
-                selection,
-                selectionArgs
-        );
 
     }
-
-    public void deleteInfo(String userName){
-
-        SQLiteDatabase db = getReadableDatabase();
-
-        String selection = UsersMaster.Users.COLUMN_NAME_USERNAME + "LIKE ?";
-        String[] selectionArgs = {userName};
-
-        db.delete(UsersMaster.Users.TABLE_NAME,selection,selectionArgs);
-    }
-
-    public List readAllInfo(){
-        SQLiteDatabase db = getReadableDatabase();
-
-        String[] projection = {
-                UsersMaster.Users._ID,
-                UsersMaster.Users.COLUMN_NAME_USERNAME,
-                UsersMaster.Users.COLUMN_NAME_PASSWORD
-        };
-
-        String sortOrder = UsersMaster.Users.COLUMN_NAME_USERNAME + "DESC";
-
-        Cursor cursor = db.query(
-                UsersMaster.Users.TABLE_NAME,
-                projection,
-                null,
-                null,
-                null,
-                null,
-                sortOrder
-        );
-
-        List userNames = new ArrayList<>();
-        List passwords = new ArrayList<>();
-
-        while (cursor.moveToNext()){
-            String username = cursor.getString(cursor.getColumnIndexOrThrow(UsersMaster.Users.COLUMN_NAME_USERNAME));
-            String password = cursor.getString(cursor.getColumnIndexOrThrow(UsersMaster.Users.COLUMN_NAME_PASSWORD));
-            userNames.add(username);
-            passwords.add(password);
-        }
-        cursor.close();
-        return userNames;
-    }
-
-    public void readInfo(String userName, String passw, Context context){
-
-        SQLiteDatabase db = getReadableDatabase();
-
-        String[] projection = {
-                UsersMaster.Users.COLUMN_NAME_USERNAME,
-                UsersMaster.Users.COLUMN_NAME_PASSWORD
-        };
-
-        String sortOrder =
-                UsersMaster.Users.COLUMN_NAME_USERNAME + "DESC";
-
-                Cursor cursor = db.query(
-                        UsersMaster.Users.TABLE_NAME,
-                        projection,
-                        null,
-                        null,
-                        null,
-                        null,
-                        sortOrder
-                );
-
-                List userNames = new ArrayList<>();
-                List passwords = new ArrayList<>();
-
-                while (cursor.moveToNext()){
-                    String username = cursor.getString(cursor.getColumnIndexOrThrow(UsersMaster.Users.COLUMN_NAME_USERNAME));
-                    String password = cursor.getString(cursor.getColumnIndexOrThrow(UsersMaster.Users.COLUMN_NAME_PASSWORD));
-
-                    userNames.add(username);
-                    passwords.add(password);
-
-                    cursor.close();
-
-                    if ((userNames.contains(userName))) {
-
-                        if ((passwords.contains(passw))) {
-
-                            Toast.makeText(context,"LOGIN SUCCESSFULLY",Toast.LENGTH_LONG).show();
-                        }else{
-                            Toast.makeText(context,"INCORRECT PASSWORD ",Toast.LENGTH_LONG).show();
-                        }
-                        }else{
-                        Toast.makeText(context,"INCORRECT USERNAME",Toast.LENGTH_LONG).show();
-                    }
-                }
-
-    }
-
-
-
-
 }
--- app/src/main/java/com/example/database_androidstudio/Database/Users.java
+++ app/src/main/java/com/example/database_androidstudio/Database/Users.java
@@ -2,4 +2,37 @@ package com.example.database_androidstudio.Database;
 
 public class Users {
 
+    private int id;
+    private String Username;
+    private String Password;
+
+    public Users(int id, String username, String password) {
+        this.id = id;
+        Username = username;
+        Password = password;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return Username;
+    }
+
+    public void setUsername(String username) {
+        Username = username;
+    }
+
+    public String getPassword() {
+        return Password;
+    }
+
+    public void setPassword(String password) {
+        Password = password;
+    }
 }
